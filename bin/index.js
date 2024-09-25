3e902d85c4cf38612b13ae2df319b876d44d5c63
jaydeetee97 <pricesmayvary302@gmail.com>
2024-09-25 11:54:51 -0400

    added ajv, better-ajv errors, debug, and a logger

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+const logger = require("../src/logger")("bin");
 const arg = require("arg");
 const chalk = require("chalk");
 const getConfig = require("../src/config/config-mgr");
@@ -10,12 +11,14 @@ try {
     "--build": Boolean,
   });
 
+  logger.debug("Received args", args);
+
   if (args["--start"]) {
     const config = getConfig();
     start(config);
   }
 } catch (error) {
-  console.log(chalk.yellow(error.message));
+  logger.warning(error.message);
   console.log();
   usage();
 }
--- src/config/config-mgr.js
+++ src/config/config-mgr.js
@@ -1,17 +1,26 @@
+const logger = require("../logger")("config:mgr");
 const chalk = require("chalk");
 const { cosmiconfigSync } = require("cosmiconfig");
+const schema = require("./schema.json");
+const betterAjvErrors = require("better-ajv-errors");
+const Ajv = require("ajv").default;
+const ajv = new Ajv({ jsonPointers: true });
 const configLoader = cosmiconfigSync("tool");
 
 module.exports = function getConfig() {
   const result = configLoader.search(process.cwd());
-
   if (!result) {
-    console.log(chalk.yellow("Could not find configuration, using default"));
-
+    logger.warning("Could not find configuration, using default");
     return { port: 1234 };
   } else {
-    console.log("Found configuration", result.config);
-
+    const isValid = ajv.validate(schema, result.config);
+    if (!isValid) {
+      logger.warning("Invalid configuration was supplied");
+      console.log();
+      console.log(betterAjvErrors(schema, result.config, ajv.errors));
+      process.exit(1);
+    }
+    logger.debug("Found configuration", result.config);
     return result.config;
   }
 };
